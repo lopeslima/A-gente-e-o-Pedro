658f6348808a318ce4a98c43449c5f33994646ca
memisju <125404782+lopeslima@users.noreply.github.com>
2023-02-27 18:00:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function bot () {
+    var usuario= document.getElementById ("caixadetexto").value;
+var camp= "Sua comida favorita é:";
+document.getElementById ("resposta").innerHTML=camp+usuario;
+}
